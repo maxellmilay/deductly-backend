066fb5865fc61003d433f6294b9376fe339992a9
Claryte <carllaw.banuag@gmail.com>
2025-01-18 02:31:21 +0800

    Added default url view & allowed all host to local

--- main/settings/local.py
+++ main/settings/local.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.254.208"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def home_view(request):
+    return HttpResponse("Welcome to Deductly!")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("apps.account.urls")),
+    path("", home_view),
 ]

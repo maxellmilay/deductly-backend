aa0a770d0f95d7f7513c3c99f6767fbad3d9d5fd
yunjin08 <jed.donaire08@gmail.com>
2025-02-07 14:30:42 +0800

    update local settings

--- main/settings/local.py
+++ main/settings/local.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.getenv("LOCAL_AREA_BASE_URL"), "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "corsheaders",
     "apps.account",
+    "apps.chatbot",
 ]
 
 MIDDLEWARE = [

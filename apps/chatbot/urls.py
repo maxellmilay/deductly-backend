9d0c1c3e32636ee9e5fd78e891c3c1ccf96e85e0
yunjin08 <jed.donaire08@gmail.com>
2025-02-09 13:15:42 +0800

    add generic api

--- apps/chatbot/urls.py
+++ apps/chatbot/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import ChatView, ChatHistoryView
 
 urlpatterns = [
-    path("chat/", ChatView.as_view(), name="chat"),
-    path("chat/history/<int:user_id>/", ChatHistoryView.as_view(), name="chat-history"),
+    path("chat/", ChatView.as_view({"get": "list", "post": "create"}), name="chat"),
+    path(
+        "chat/history/<int:user_id>/",
+        ChatHistoryView.as_view({"get": "list", "post": "create"}),
+        name="chat-history",
+    ),
 ]
--- apps/chatbot/views.py
+++ apps/chatbot/views.py
@@ -8,12 +8,21 @@ from .models import Chat
 from .serializers import ChatSerializer
 from .utils.main import generate_answer
 from apps.account.models import CustomUser
+from django.contrib.auth import get_user_model
 from datetime import datetime
+from main.utils.generic_api import GenericView
+from django.core.paginator import Paginator
+
+User = get_user_model()
 
 
-class ChatView(APIView):
+class ChatView(GenericView):
     def post(self, request):
+        print("Request data:", request.data)
         question = request.data.get("question")
+        user_id = request.data.get("id")
+        print(user_id, question, "user id")
+
         if not question:
             return Response(
                 {"error": "Question is required"}, status=status.HTTP_400_BAD_REQUEST
@@ -23,11 +32,9 @@ class ChatView(APIView):
             final_answer = generate_answer(question)
 
             # Save to database
-            user = (
-                CustomUser.objects.get(pk=request.user.id)
-                if request.user.is_authenticated
-                else None
-            )
+            user = CustomUser.objects.get(pk=user_id) if user_id else None
+
+            print(user, "user printing")
 
             if user:
                 chat = Chat.objects.create(
@@ -48,13 +55,19 @@ class ChatView(APIView):
             )
 
 
-class ChatHistoryView(APIView):
-    def get(self, request, user_id):
-        try:
-            chats = Chat.objects.filter(user_id=user_id).order_by("-timestamp")
-            serializer = ChatSerializer(chats, many=True)
-            return Response(serializer.data)
-        except CustomUser.DoesNotExist:
-            return Response(
-                {"error": "User not found"}, status=status.HTTP_404_NOT_FOUND
-            )
+class ChatHistoryView(GenericView):
+    queryset = Chat.objects.filter().order_by("-timestamp")
+    serializer_class = ChatSerializer
+    allowed_methods = ["get", "list", "create"]
+    # def filter(self, top, bottom):
+    #     try:
+    #         chats = Chat.objects.filter(user_id=user_id).order_by("-timestamp")
+    #         paginator = Paginator(chats, self.size_per_request)
+    #         page_number = (top // self.size_per_request) + 1
+    #         page = paginator.get_page(page_number)
+    #         serializer = ChatSerializer(chats, many=True)
+    #         return Response(serializer.data)
+    #     except CustomUser.DoesNotExist:
+    #         return Response(
+    #             {"error": "User not found"}, status=status.HTTP_404_NOT_FOUND
+    #         )

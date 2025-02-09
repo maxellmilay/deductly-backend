a5c201d233496e887a76727cfbd9162868843c50
yunjin08 <jed.donaire08@gmail.com>
2025-02-09 16:36:00 +0800

    modified urls

--- apps/chatbot/urls.py
+++ apps/chatbot/urls.py
@@ -2,9 +2,13 @@ from django.urls import path
 from .views import ChatView, ChatHistoryView
 
 urlpatterns = [
-    path("chat/", ChatView.as_view({"get": "list", "post": "create"}), name="chat"),
     path(
-        "chat/history/<int:user_id>/",
+        "chat/<int:pk>/",
+        ChatView.as_view({"get": "list", "post": "create"}),
+        name="chat",
+    ),
+    path(
+        "chat/history/",
         ChatHistoryView.as_view({"get": "list", "post": "create"}),
         name="chat-history",
     ),

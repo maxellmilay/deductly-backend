ef60351f4093ca2936e944216ff0cd6d132967c5
Maxell Milay <milaymaxell@gmail.com>
2025-06-04 09:38:13 +0800

    added deployment scripts and config

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,23 @@
 FROM python:3.12-slim
 
 # Set environment variables
-ENV PYTHONDONTWRITEBYTECODE 1
-ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+ENV DEBUG=True
+ENV IN_DOCKER=1
+ENV ENVIRONMENT=local
+
+# Install system dependencies for OpenCV
+RUN apt-get update && apt-get install -y \
+    libglib2.0-0 \
+    libsm6 \
+    libxext6 \
+    libxrender-dev \
+    libgomp1 \
+    libgl1-mesa-glx \
+    libglib2.0-0 \
+    libgtk-3-0 \
+    && rm -rf /var/lib/apt/lists/*
 
 # Set work directory
 WORKDIR /app
@@ -15,8 +30,17 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Copy project files
 COPY . /app/
 
+# Create static files directory and set permissions
+RUN mkdir -p /app/staticfiles && chmod -R 755 /app/staticfiles
+
+# Collect static files
+RUN python manage.py collectstatic --noinput
+
+# Set the Python path to include the directory
+ENV PYTHONPATH=/app
+
 # Expose the port
 EXPOSE 8080
 
 # Start Gunicorn server
-CMD ["gunicorn", "--bind", "0.0.0.0:8080", "main.wsgi:application"]
+CMD ["gunicorn", "--bind", "0.0.0.0:8080", "main.wsgi:application", "--timeout", "120"]
--- main/settings/local.py
+++ main/settings/local.py
@@ -89,10 +89,21 @@ WSGI_APPLICATION = "main.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
@@ -131,7 +142,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -146,7 +158,11 @@ AUTH_USER_MODEL = "account.CustomUser"
 CORS_ALLOW_ALL_ORIGINS = True
 
 # CSRF
-CSRF_TRUSTED_ORIGINS = ["http://localhost:8000", "http://127.0.0.1:8000"]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "https://deductly.maxellmilay.com",
+]
 CSRF_COOKIE_SECURE = False
 CSRF_USE_SESSIONS = False
 CSRF_COOKIE_HTTPONLY = False
